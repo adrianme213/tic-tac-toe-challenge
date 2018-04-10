4026ffc5c9d55e0eb2c0866c9296b934ef8e09f9
Adrian Meza <adrianme213@gmail.com>
2018-04-09 19:33:36 -0700

    [feat] - majorDiagonal check - minorDiagonal check

--- client/src/grid.js
+++ client/src/grid.js
@@ -26,7 +26,7 @@ Grid.prototype.insertMove = function(row, col, value) {
 
 Grid.prototype.rowSum = function() {
   for (let ii = 0; ii < this.grid.length; ii++) {
-    const totalForRow = this.grid[i].reduce((total, value) => {
+    const totalForRow = this.grid[ii].reduce((total, value) => {
       return total + value;
     }, 0);
     if (totalForRow === 3 || totalForRow === -3) {
@@ -39,9 +39,9 @@ Grid.prototype.rowSum = function() {
 
 Grid.prototype.colSum = function() {
   for (let ii = 0; ii < this.grid.length; ii++) {
-    const totalForCol = 0;
+    let totalForCol = 0;
     this.grid.forEach((row, index) => {
-      totalForCol += this.grid[index][row];
+      totalForCol += this.grid[index][ii];
     });
     if (totalForCol === 3 || totalForCol === -3) {
       // Positive or Negative player has won so we return that value
@@ -50,3 +50,34 @@ Grid.prototype.colSum = function() {
   }
   return false;
 }
+
+Grid.prototype.majorDiagonal = function() {
+  let total = 0;
+  for (let ii = 0; ii < this.grid.length; ii++) {
+    total += this.grid[ii][ii];
+  }
+  if (total === 3 || total === -3) {
+    // Positive or Negative player has won so we return that value
+    return total;
+  }
+  return false;
+}
+
+
+Grid.prototype.minorDiagonal = function() {
+  let total = 0;
+  for (let ii = 0; ii < this.grid.length; ii++) {
+    total += this.grid[ii][(this.grid.length - 1) - ii];
+  }
+  if (total === 3 || total === -3) {
+    // Positive or Negative player has won so we return that value
+    return total;
+  }
+  return false;
+}
+
+/*
+To run program, players alternate turns and then run the helper functions
+to check if anyone has one. If insertMove is not valid, same player is asked
+to enter a row and column.
+/*
